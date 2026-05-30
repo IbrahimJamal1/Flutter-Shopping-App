4de2d6fc6885ed652b709d799fbf547c752e1d45
Ibrahim Jamal <ibrahimgamal932@gmail.com>
2026-05-30 16:16:57 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ import 'package:ecommerce/register.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce/providerapp.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
 
 void main() {
